27684d00574bc10bc03b51f727447bbf689103f0
Madhaneey <madhanrajs047@gmail.com>
2025-07-26 23:06:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS, {
-  host : process.env.DB_HOST,
-  dialect: 'mysql',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true
+      }
+    }
+  }
+);
+
 module.exports = sequelize;
- 
