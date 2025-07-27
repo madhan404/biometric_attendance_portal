7fa7121e189ea6b748112abb5affd7f433aefa67
Madhaneey <madhanrajs047@gmail.com>
2025-07-27 15:16:46 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase chunk size warning threshold (in KB)
+    chunkSizeWarningLimit: 1500,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Put all node_modules code into a separate chunk named 'vendor'
+            return 'vendor'
+          }
+          // Add more manual chunking logic if needed for your application
+        }
+      }
+    }
+  }
 })
